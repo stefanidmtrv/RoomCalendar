15662bfec443adc6253c0b610a74af30496fb9e2
stefanidmtrv <dimitrovastefani00@gmail.com>
2022-01-08 12:20:47 +0000

    small layout change

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -13,10 +13,19 @@
                     @csrf
 
                     <div>
-                        <p>Date: {{ $date }}</p>
-                        <p>From: {{$slot1}} to {{$slot2}}</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-clock" viewBox="0 0 16 16">
+                            <path
+                                d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
+                            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
+                        </svg>
+                        <strong>Date:</strong> {{ $date }}
+                        <p>
+                            &emsp; <strong>Time:</strong>
+                            {{ $slot1 }} - {{ $slot2 }}
+                        </p>
                     </div>
-                    
+
                     <div class="form-group mb-4">
                         <p>Add a title: </p>
                         <input type="text" name="name" value="{{ old('name') }}" class="form-control"></p>
@@ -24,20 +33,21 @@
 
                     <div class="form-group mb-4">
                         <p>Add a description: </p>
-                        <input type="text" name="description" value="{{ old('description') }}" class="form-control"></p>
+                        <input type="text" name="description" value="{{ old('description') }}" class="form-control">
+                        </p>
                     </div>
 
                     <div class="col-md-12 text-center">
                         <button type="submit" class="btn btn-success" value="Submit">Submit</button>
-                        <a class="btn btn-secondary" href="{{ route('rooms.index') }}">Cancel</a> 
+                        <a class="btn btn-secondary" href="{{ route('rooms.index') }}">Cancel</a>
                     </div>
 
-                    
-                    
-                    <input type="hidden" name="start_date_time" value="{{ $date.$slot1 }}" />
-                    <input type="hidden" name="end_date_time" value="{{ $date.$slot2 }}" />
-                    
-                    
+
+
+                    <input type="hidden" name="start_date_time" value="{{ $date . $slot1 }}" />
+                    <input type="hidden" name="end_date_time" value="{{ $date . $slot2 }}" />
+
+
                 </form>
             </div>
         </div>
