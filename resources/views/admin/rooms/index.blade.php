e776c2d3e39d7db25c1b335fc2070ce2e459aff1
stefanidmtrv <dimitrovastefani00@gmail.com>
2022-01-17 12:52:08 +0000

    add device in admin section

--- resources/views/admin/rooms/index.blade.php
+++ resources/views/admin/rooms/index.blade.php
@@ -9,14 +9,14 @@
     <x-slot name="slot">
 
         <a class="btn btn-outline-secondary" href="{{ route('home') }}">Go Back</a>
-        
+        <a href="{{ route('displays.create') }} " type="button" class="btn btn-success">Add a new device</a>
+
         <div class="d-grid gap-2 col-6 mx-auto">
             @foreach ($rooms as $room)
                 <a href="{{ route('rooms.show', ['room' => $room]) }} " type="button" class="btn btn-primary">Room
                     {{ $room->id }}</a>
             @endforeach
         </div>
-
 
     </x-slot>
 
