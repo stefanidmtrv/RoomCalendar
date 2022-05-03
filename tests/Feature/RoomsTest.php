d00e8176ea52335cc06bd8847eaabdfa28d0e6e9
stefanidmtrv <dimitrovastefani00@gmail.com>
2022-05-03 13:22:26 +0100

    deleting comments

--- tests/Feature/RoomsTest.php
+++ tests/Feature/RoomsTest.php
@@ -22,8 +22,6 @@ class RoomsTest extends TestCase
 
     $response->assertStatus(403);
 
-    // PASS  Tests\Feature\ExampleTest
-    // ✓ example
     }
 
     public function test_example_2()
@@ -32,12 +30,7 @@ class RoomsTest extends TestCase
     $response = $this->post('/admin/event/store');
 
     $response->assertStatus(200);
-    // • Tests\Feature\RoomsTest > example 2
-    // Expected response status code [200] but received 403.
-    // Failed asserting that 200 is identical to 403.
+  
 }
 }
-// PASS  Tests\Feature\Auth\AuthenticationTest
-// ✓ login screen can be rendered
-// ✓ users can authenticate using the login screen
-// ✓ users can not authenticate with invalid password
+
